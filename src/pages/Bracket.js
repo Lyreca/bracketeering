fefd78bbef33acaf8e33dc42a8a7274701655276
Lyreca <fsantoso@liberty.edu>
2023-03-03 09:36:06 -0500

    first implementation of passing in state variables

--- src/pages/Bracket.js
+++ src/pages/Bracket.js
@@ -1,12 +1,17 @@
 import logo from '../assets/logo.svg';
-import bracketName from './App.js';
+import { useLocation } from 'react-router-dom';
 
 const Bracket = () => {
+  const {state} = useLocation();
+  const {name, size, type} = state;
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Created Bracket</h1>
-        <p>Bracket Name: {bracketName}</p>
+        <p>Bracket Name: {name}</p>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
