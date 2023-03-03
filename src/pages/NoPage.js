58c206b4a32d3e7ec87720da48a693a65f1e0483
Lyreca <fsantoso@liberty.edu>
2023-03-03 11:02:21 -0500

    styled no page

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,14 @@
+import '../styles/NoPage.css'
+import { Link } from 'react-router-dom'
+
 const NoPage = () => {
-    return <h1>404</h1>;
+    return (
+    <div className="nopage">
+        <h1>404</h1>
+        <p>Page not found!</p>
+        <p>Return to <Link to='/'>Home</Link></p>
+    </div>
+    );
   };
   
 export default NoPage;
