0c5dcc395ce1603a9e1ad520bb01dca8253df676
jplohman01 <jplohman@liberty.edu>
2023-03-03 23:37:55 -0500

    Randomizer

--- src/pages/Bracket.js
+++ src/pages/Bracket.js
@@ -2,6 +2,26 @@ import { useEffect, useState } from 'react';
 import '../styles/Bracket.css';
 import { useLocation } from 'react-router-dom';
 
+const shuffle = (array) => {
+  // Implementation of Fisher-Yates shuffle algorithm
+  let currentIndex = array.length;
+  let temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+};
+
 const Bracket = () => {
   const {state} = useLocation();
   const {name, size, type} = state;
@@ -9,8 +29,9 @@ const Bracket = () => {
 
   useEffect(() => {
     let newBracket = [];
+    const shuffledType = shuffle(type);
     for (let i = 0; i < size; i++) {
-      newBracket.push(type[i]);
+      newBracket.push(shuffledType[i]);
     }
     setBracket(newBracket);
   }, [size]);
